8c07ae206883fac54efdf3f8a7d91c74f710af1b
AizeRs <NAKorshunov@mail.ru>
2021-12-17 20:17:18 +0500

    start making game5

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -2,6 +2,10 @@ package battleship;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Battleship");
+        Ship[] ships = new Ship[1];
+        ships[0] = new Ship("F1", "F5");
+        Board board = new Board(5);
+        board.printBoard();
+
     }
 }
